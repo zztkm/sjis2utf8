faa16fe8ec23d4684581b3e1826ba7e305ca0166
TakumiTsuruta <ttsuruta@landscape.co.jp>
2023-10-25 18:47:55 +0900

    ざっと実装はできた

--- go.mod
+++ go.mod
@@ -2,3 +2,4 @@ module github.com/zztkm/app
 
 go 1.21
 
+require golang.org/x/text v0.13.0
--- main.go
+++ main.go
@@ -1,24 +1,30 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
 	"fmt"
+	"io"
+	"log"
 	"os"
+	"path/filepath"
+
+	"golang.org/x/text/encoding/japanese"
+	"golang.org/x/text/transform"
 )
 
 const version = "0.0.1"
 
 var revision = "HEAD"
 
-func fatal(err error) {
-	fmt.Fprintf(os.Stderr, "%s\n", err)
-	os.Exit(1)
+func getFileNameWithoutExt(path string) string {
+	return filepath.Base(path[:len(path)-len(filepath.Ext(path))])
 }
-
 
 func main() {
 	var showVersion bool
 	var showHelp bool
+	var output string
 
 	// options
 	fs := flag.NewFlagSet("go-cli-template", flag.ExitOnError)
@@ -26,14 +32,16 @@ func main() {
 	fs.BoolVar(&showVersion, "v", false, "show version")
 	fs.BoolVar(&showHelp, "help", false, "show help")
 	fs.BoolVar(&showHelp, "h", false, "show help")
+	fs.StringVar(&output, "o", "", "write output to file")
+	fs.StringVar(&output, "output", "", "write output to file")
 	fs.Usage = func() {
 		fmt.Println(`Usage:
- app <path>
+ sjis2utf8 [options] [<input>]
 
 Flags:`)
 		fs.PrintDefaults()
 		fmt.Println(`Repository:
-  https://github.com/zztkm/app`)
+  https://github.com/zztkm/sjis2utf8`)
 	}
 
 	fs.Parse(os.Args[1:])
@@ -45,5 +53,43 @@ Flags:`)
 	if showHelp {
 		fs.Usage()
 		return
+	}
+
+	if fs.NArg() < 1 {
+		log.Fatal("[ERROR] path is required")
+	}
+
+	path := fs.Arg(0)
+
+	f, err := os.Open(path)
+	if err != nil {
+		log.Fatalf("[ERROR] %s", err)
+	}
+	defer f.Close()
+
+	var writer io.Writer
+	if output != "" {
+		f, err := os.Create(output)
+		if err != nil {
+			log.Fatalf("[ERROR] %s", err)
+		}
+		defer f.Close()
+		writer = f
+	} else {
+		writer = os.Stdout
+	}
+
+	r := csv.NewReader(transform.NewReader(f, japanese.ShiftJIS.NewDecoder()))
+	w := csv.NewWriter(writer)
+	for {
+		records, err := r.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatalf("[ERROR] %s", err)
+		}
+		if err := w.Write(records); err != nil {
+			log.Fatalf("[ERROR] %s", err)
+		}
 	}
 }
